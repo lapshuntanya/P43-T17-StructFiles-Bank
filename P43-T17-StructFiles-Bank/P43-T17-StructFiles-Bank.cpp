7a0fbb188cc72523c14e30a6bb27b72cd691c643
lapshun_t_s <lapshun.tatiana@gmail.com>
2025-06-11 18:24:36 +0300

    main array

--- P43-T17-StructFiles-Bank/P43-T17-StructFiles-Bank.cpp
+++ P43-T17-StructFiles-Bank/P43-T17-StructFiles-Bank.cpp
@@ -1,14 +1,14 @@
 ﻿#include <iostream>
 #include "ArrayTemplate.h"
 #include "Card.h"
-#define CREATE_LOAD 0 // 1- Create, 0- Load
+#define CREATE_LOAD 1 // 1- Create, 0- Load
 
 int main()
 {
     srand(time(0));
     FILE* file = nullptr;
 
-    Card Dmytro; //name="undefined", number=0, cash=0
+ /*   Card Dmytro; //name="undefined", number=0, cash=0
 
 #if CREATE_LOAD == 1
     Dmytro.createCard();
@@ -34,6 +34,25 @@ int main()
         Dmytro.saveToTextFile(file);
         fclose(file);
         cout << "Saved!\n";
-    }
+    }*/
+
+    int NClients = 0; // Розмір масиву
+    Card* arr = nullptr; // Масив карток
+
+  //================= Create or Load ====================
+#if CREATE_LOAD == 1
+    NClients = 3;
+    arr = new Card[3]{
+        {"Lesya Ukrainka", 1234567, 200},
+        {"Bogdan Khmelnytskiy", 6543212, 5'000},
+        {"Taras Shevchenko", 876543, 100}
+    };
+#else
+    //load
+#endif
+  //====================== Menu =========================
+  
+
+  //====================== Save =========================
 }
  
